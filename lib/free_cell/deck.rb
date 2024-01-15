4c61fd0a1456a08f1b8ecab5386cb4cb8a0e2e9b
candiceinman <candiceinman2@gmail.com>
2024-01-14 20:33:53 -0400

    add Deck.parse method

--- lib/free_cell/deck.rb
+++ lib/free_cell/deck.rb
@@ -2,4 +2,12 @@ class Deck < Array
   def initialize
     super(52, &Card.method(:new))
   end
+
+  def self.parse(input)
+    cards = input.split(/\s+/).map { Card.parse(_1) }
+    raise ArgumentError, "invalid: expected 52 cards, got #{cards.size}" if cards.size != 52
+    raise ArgumentError, "invalid: expected unique cards" if cards != cards.uniq(&:to_s)
+
+    cards
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -16,11 +16,43 @@ describe Deck do
 
     it "must have all unique elements" do
       deck = Deck.new
-      assert_equal deck, deck.uniq
+      assert_equal deck, deck.uniq(&:to_s)
     end
 
     it "must contain only cards" do
       assert Deck.new.all?(Card), "Expected all elements to be of type Card"
+    end
+  end
+
+  describe "when parsing cards" do
+    it "must return an array of 52 unique cards" do
+      faces = ["a", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"]
+      suits = ["s", "h", "c", "d"]
+      input = faces.product(suits).map(&:join).join(" ")
+
+      cards = Deck.parse(input)
+      assert_equal 52, cards.size
+      assert_equal cards, cards.uniq(&:to_s)
+      assert_equal "A♠", cards.first.to_s
+      assert_equal "K♦", cards.last.to_s
+    end
+
+    it "must raise an exception if there isn't 52 cards" do
+      assert_raises_with_message(ArgumentError, "invalid: expected 52 cards, got 0") { Deck.parse("") }
+      assert_raises_with_message(ArgumentError, "invalid: expected 52 cards, got 51") { Deck.parse("as " * 51) }
+      assert_raises_with_message(ArgumentError, "invalid: expected 52 cards, got 53") { Deck.parse("as " * 53) }
+    end
+
+    it "must raise an exception if there are duplicate cards" do
+      assert_raises_with_message(ArgumentError, "invalid: expected unique cards") { Deck.parse("as " * 52) }
+    end
+
+    it "must raise an exception if there are any invalid cards" do
+      assert_raises_with_message(ArgumentError, "invalid: 11f") { Deck.parse("11f") }
+      assert_raises_with_message(ArgumentError, "invalid: -1d") { Deck.parse("-1d") }
+      assert_raises_with_message(ArgumentError, "invalid: 20h") { Deck.parse("20h") }
+      assert_raises_with_message(ArgumentError, "invalid: 0h") { Deck.parse("0h") }
+      assert_raises_with_message(ArgumentError, "invalid: 9e") { Deck.parse("9e") }
     end
   end
 end
