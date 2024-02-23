eeebb2d4159f96eb0bf204be5121295206d9e895
Alex Inman <alex.inman@introhive.com>
2024-02-22 21:42:52 -0400

    Location test

---
+++ lib/location.rb
@@ -0,0 +1,17 @@
+class Location
+  # Hi Candice, this is the first class you'll need to implement.
+  # All functionality is described in the tests in location_test.rb.
+  # You'll need to implement the following methods:
+  # - initialize(cards)
+  # - add(cards)
+  # - add?(cards)
+  # - remove(number)
+  # - remove?(number)
+  # - cards
+  # - empty?
+  # - ==(other)
+  # - eql?(other)
+  # - hash
+  # And you can validate your implementation by running `rake`.
+  # Good luck!
+end
