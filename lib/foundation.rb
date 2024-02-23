cb95690c6ae1602c9ac9fdff16c0f26fd9efc565
Alex Inman <alex.inman@introhive.com>
2024-02-22 22:50:32 -0400

    Foundation test

---
+++ lib/foundation.rb
@@ -0,0 +1,10 @@
+class Foundation < Location
+  # Hi Candice, this is the third class you'll need to implement.
+  # All functionality is described in the tests in foundation_test.rb.
+  # You'll need to implement the following methods:
+  # - initialize
+  # - add?(cards)
+  # - remove?(number)
+  # And you can validate your implementation by running `rake`.
+  # Good luck!
+end
