f59e5d2005fe6ef63945ecf84d850237f2902f46
Alex Inman <alex.inman@introhive.com>
2024-02-23 14:40:22 -0400

    State test

---
+++ lib/state.rb
@@ -0,0 +1,15 @@
+class State
+  # Hi Candice, this is the fifth class you'll need to implement.
+  # All functionality is described in the tests in cascade_test.rb.
+  # You'll need to implement the following methods:
+  # - initialize(cells, foundations, cascades)
+  # - self.build(deck)
+  # - cells
+  # - foundations
+  # - cascades
+  # - ==(other)
+  # - eql?(other)
+  # - hash
+  # And you can validate your implementation by running `rake`.
+  # Good luck!
+end
