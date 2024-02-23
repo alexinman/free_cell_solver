2e89e186bb72ba3af89e938df74040ede98f941b
Alex Inman <alex.inman@introhive.com>
2024-02-22 23:56:30 -0400

    Cascade test

---
+++ lib/cascade.rb
@@ -0,0 +1,9 @@
+class Cascade < Location
+  # Hi Candice, this is the fourth class you'll need to implement.
+  # All functionality is described in the tests in cascade_test.rb.
+  # You'll need to implement the following methods:
+  # - add?(cards)
+  # - remove?(number)
+  # And you can validate your implementation by running `rake`.
+  # Good luck!
+end
