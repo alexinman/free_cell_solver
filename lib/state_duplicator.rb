aaee7d790db95a5f829b05ad1e8aa923b8483881
Alex Inman <alex.inman@introhive.com>
2024-02-25 00:05:26 -0400

    StateDuplicator test

---
+++ lib/state_duplicator.rb
@@ -0,0 +1,8 @@
+module StateDuplicator
+  # Hi Candice, this is the second module you'll need to implement.
+  # All functionality is described in the tests in state_duplicator_test.rb.
+  # You'll need to implement the following method:
+  # - dup(state)
+  # And you can validate your implementation by running `rake`.
+  # Good luck!
+end
