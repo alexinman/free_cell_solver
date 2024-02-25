a7ac5a1077ae56e061e84195b41a2f054a99daf3
Alex Inman <alex.inman@introhive.com>
2024-02-24 22:41:06 -0400

    Location

--- lib/location.rb
+++ lib/location.rb
@@ -1,8 +1,4 @@
 class Location
-  # Hi Candice, this is the first class you'll need to implement.
-  # All functionality is described in the tests in location_test.rb.
-  # You'll need to implement the following methods:
-
   def initialize(cards_in = [])
     self.cards = cards_in.dup
   end
@@ -63,7 +59,4 @@ class Location
   private
 
   attr_writer :cards
-
-  # And you can validate your implementation by running `rake`.
-  # Good luck!
 end
