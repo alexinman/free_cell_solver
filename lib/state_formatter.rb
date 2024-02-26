a7c388ecd66e1201a552086df5bd578d5df099a1
Alex Inman <alex.inman@introhive.com>
2024-02-26 10:41:44 -0400

    StateFormatter private methods '

--- lib/state_formatter.rb
+++ lib/state_formatter.rb
@@ -1,21 +1,25 @@
 module StateFormatter
-  def self.format(state)
-    <<~HEREDOC.gsub(/ +\n/, "\n")
-      #{"═" * 32}
-      #{format_single_card_locations(state.foundations)} #{format_single_card_locations(state.cells)}
+  class << self
+    def format(state)
+      <<~HEREDOC.gsub(/ +\n/, "\n")
+        #{"═" * 32}
+        #{format_single_card_locations(state.foundations)} #{format_single_card_locations(state.cells)}
 
-      #{format_cascades(state.cascades)}
-      #{"═" * 32}
-    HEREDOC
-  end
+        #{format_cascades(state.cascades)}
+        #{"═" * 32}
+      HEREDOC
+    end
 
-  def self.format_single_card_locations(locations)
-    locations.map { |f| f.cards.last.to_s.rjust(3) }.join(" ")
-  end
+    private
 
-  def self.format_cascades(cascades)
-    19.times.map do |i|
-      cascades.map { |cascade| cascade.cards[i].to_s.rjust(3) }.join(" ")
-    end.join("\n")
+    def format_single_card_locations(locations)
+      locations.map { |f| f.cards.last.to_s.rjust(3) }.join(" ")
+    end
+
+    def format_cascades(cascades)
+      19.times.map do |i|
+        cascades.map { |cascade| cascade.cards[i].to_s.rjust(3) }.join(" ")
+      end.join("\n")
+    end
   end
 end
